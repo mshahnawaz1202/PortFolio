8ad716293f97f0a99cd923ae32b5524dda901b76
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-25 12:34:06 +0500

    feat: implement routing with main pages (Home, About, Projects, Contact) and create index file for page exports

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,10 +1,20 @@
-import { NavLink } from "react-router-dom"
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import{About,Projects,Home,Contact} from './pages/'
 function App() {
 
 
   return (
     <>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+      </Router>
     </>
   )
 }
