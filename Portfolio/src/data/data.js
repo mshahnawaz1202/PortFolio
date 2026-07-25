8b1f6db7d0f22d7ac4f8940399d950b75de1bccc
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-25 11:43:52 +0500

    feat: add projects data for portfolio showcase

---
+++ Portfolio/src/data/data.js
@@ -0,0 +1,77 @@
+const projects = [
+    {
+        id:1,
+        title: "Tic Tac Toe",
+        description:"This project is a console-based Tic-Tac-Toe game developed in C++. The application allows two players to play Tic-Tac-Toe using a clean text-based interface. It also includes persistent score tracking, match history storage, leaderboard functionality, and highest scorer statistics through file handling.",
+        github:"https://github.com/mshahnawaz1202/TicTacToe",
+        TechUsed : "C++"
+    },
+
+    {
+        id:2,
+        title : "Contact Book Application",
+        description:"",
+        github : "https://github.com/mshahnawaz1202/Contact-Book-Application",
+        TechUsed : "C++, OOP"
+    },
+    {
+        id:3,
+        title:"Mini Instagram",
+        description:"This is a C++ project that simulates a simplified version of Instagram. It includes core functionalities such as user registration, login, sending messages, managing posts, and handling friend requests. The program runs in a terminal environment and uses Windows-specific libraries for UI enhancements.",
+        github: "https://github.com/mshahnawaz1202/Mini-Instagram",
+        TechUsed : "C++, Data structure"
+
+    },
+    {
+        id:4,
+        title:"Mini Operating System",
+        description:"This is a console-based mini operating system developed in C++. It simulates core operating system functionalities like process creation, termination, memory management, and includes various utility applications.",
+        github:"https://github.com/mshahnawaz1202/Mini-Operating-System",
+        TechUsed : "C++ , OS concepts"
+    },
+    {
+        id:5,
+        title:"Mini Power BI",
+        description:"This project is a Power BI–style Data Analytics Dashboard built with Streamlit , offering a complete end-to-end data workflow: Upload,Clean,Visualize,Train ML models,Predict,Chat with your dataset => All in one interactive web application —> no coding required for the user.",
+        github : "https://github.com/mshahnawaz1202/MiniPowerBI",
+        TechUsed : "Python, Streamlit, Numpy, Pandas,Scikit-learn, Matplotlib"
+    },
+    {
+        id:6,
+        title:"Arabic Poetry Management",
+        description : "A sophisticated, multi-layered Desktop Application for managing, analyzing, and exploring Arabic poetry. Built with JavaFX and a robust 3-Tier Architecture, this system integrates advanced NLP capabilities to provide deep insights into poetic structures.",
+        github: "https://github.com/mshahnawaz1202/Arabic-Poetry-Management-System",
+        TechUsed : "Java, JavaFx, MySQL, Design Priciples and Patterns"
+
+    },
+    {
+        id:7,
+        title:'BOS Optics',
+        description:'BOS Optics is a full-stack Django e-commerce platform developed for an optical store. It enables customers to browse premium eyewear, search products, manage shopping carts, place orders, book eye-care appointments, and read informative blog posts.',
+        github:"https://github.com/mshahnawaz1202/BOS-Optics",
+        TechUsed:"Python, Django, Javascript,SQLite, HTML5, CSS3, uv(Package Manager)"
+    },
+    {
+        id:7,
+        title:"StayFinder",
+        description:"A full-featured property booking app (Airbnb-style) built with Kotlin, Jetpack Compose + Views, Firebase, and Room/SQLite.",
+        github:"https://github.com/mshahnawaz1202/Stay-Finder-Airbnb-Inspired-Property-Rental-Application-",
+        TechUsed:"Kotlin, "
+    },
+    {
+        id:8,
+        title:"DALGONA CHALLENGE",
+        description:"A terminal-based DalGona (shape guessing) game implemented in Bash. Players identify ASCII shapes within a time limit and earn points. This project includes single-player, vs-computer, and multiplayer modes.",
+        github:"https://github.com/mshahnawaz1202/DALGONA-CHALLENGE",
+        TechUsed:"Bash, WSL2"
+    },
+    {
+        id:9,
+        title:"TesOps and Automation Framework",
+        description:"A comprehensive BDD (Behavior-Driven Development) Test Automation Framework for Web UI testing built with Java, Selenium WebDriver, Cucumber, and Allure Reports.",
+        github:"https://github.com/mshahnawaz1202/TestOps-andAutomation-Framework",
+        TechUsed:"Java, Selenium, Cucumber"
+    }
+
+
+]
