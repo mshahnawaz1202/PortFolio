644afe27c258cf17c3b645f4a69cb7f0d8b4d451
Muhammad Shah Nawaz <shahnawaz.swz1202@gmail.com>
2026-07-25 11:48:09 +0500

    feat: add social links data for portfolio

---
+++ Portfolio/src/data/social_links.js
@@ -0,0 +1,9 @@
+const links = [
+    {
+        github : "https://github.com/mshahnawaz1202",
+        instagram : "https://www.instagram.com/m.shahnawaz_1202/",
+        linkedin : "https://www.linkedin.com/in/muhammad-shah-nawaz-se/",
+        email : "shahnawaz.swz1202@gmail.com"
+
+    }
+]
